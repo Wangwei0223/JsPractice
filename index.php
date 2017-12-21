609aab63e31a70dc166f26081cacb15178cff281
WeiWang0223 <ww1306@nyu.edu>
2017-12-20 22:05:07 -0500

    upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>JS Exercise</title>
+    <link rel="stylesheet" type="text/css" href="./static/css/bootstrap.min.css">
+    <script type="text/javascript" src="./static/js/jquery-3.2.1.min.js"></script>
+</head>
+
+<style type="text/css">
+    body{
+         background-color: gray;
+    }
+</style>
+
+<body>
+
+<button class="btn btn-warning" id="btn1">Test</button>
+
+</body>
+</html>
+
+<script type="text/javascript">
+    $('#btn1').click(function(){
+        alert('WC');
+    });
+</script>
